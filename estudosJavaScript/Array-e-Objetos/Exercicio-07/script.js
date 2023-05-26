bbbe6af65329f5fdb59a4c602b81b5d7f84109ef
CaioPaulinoCosta <caiopaulinocosta@hotmail.com>
2023-05-26 16:17:11 -0300

    Exercício 07 | Arrays e Objetos | Realizado

---
+++ estudosJavaScript/Array-e-Objetos/Exercicio-07/script.js
@@ -0,0 +1,11 @@
+let json = {
+    "nome": "Caio",
+    "idade": "18",
+    "cidade": "Ourinhos",
+    "curso": "ADS"
+};
+
+console.log(json.nome);
+console.log(json.idade);
+console.log(json.cidade);
+console.log(json.curso);
