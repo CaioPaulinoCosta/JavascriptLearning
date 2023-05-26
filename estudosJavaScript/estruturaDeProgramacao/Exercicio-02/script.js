b554d9339205880d6a87ffb769252e675e9ac723
CaioPaulinoCosta <caiopaulinocosta@hotmail.com>
2023-05-26 00:07:22 -0300

    Exercício 02 | Estrutura de Programação | Realizado

---
+++ estudosJavaScript/estruturaDeProgramacao/Exercicio-02/script.js
@@ -0,0 +1,17 @@
+let idade = 18;
+
+if (idade >= 18) {
+    console.log("Entrada liberada!");
+} else {
+    console.log("Poribido de entrar!");
+}
+
+//
+
+let idade2 = 15;
+
+if (idade2 >= 18) {
+    console.log("Entrada liberada!");
+} else {
+    console.log("Poribido de entrar!");
+}
