6284dc8032bd8f6402b5e3f4b1bb25eb6a0e8b01
CaioPaulinoCosta <caiopaulinocosta@hotmail.com>
2023-05-26 16:08:06 -0300

    Exercício 05 | Arrays e Objetos | Realizado

---
+++ estudosJavaScript/Array-e-Objetos/Exercicio-05/script.js
@@ -0,0 +1,14 @@
+let arr1 = ["Caio", "Jessica", "Patricia", "Jefferson", "Nice"];
+let arr2 = ["Caio", "Jessica"];
+
+if (arr1.length == 5) {
+    console.log("Muitos elementos");
+} else { 
+    console.log("Poucos elementos");
+}
+
+if (arr2.length == 5) {
+    console.log("Muitos elementos");
+} else { 
+    console.log("Poucos elementos");
+}
