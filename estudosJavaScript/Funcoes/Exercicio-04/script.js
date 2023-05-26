b390a3169f70621cedd62dce629be7da9ae600fc
CaioPaulinoCosta <caiopaulinocosta@hotmail.com>
2023-05-26 09:56:22 -0300

    Exercício 04 | Estrutura de Função | Realizado

---
+++ estudosJavaScript/Funcoes/Exercicio-04/script.js
@@ -0,0 +1,20 @@
+function randomNum (num){
+    return Math.floor(Math.random() * num) + 1;
+}
+
+console.log(randomNum(10));
+console.log(randomNum(10));
+console.log(randomNum(10));
+console.log(randomNum(10));
+console.log(randomNum(10));
+console.log(randomNum(10));
+
+//
+console.log(randomNum(50));
+console.log(randomNum(50));
+console.log(randomNum(50));
+console.log(randomNum(50));
+console.log(randomNum(50));
+console.log(randomNum(50));
+console.log(randomNum(50));
+
