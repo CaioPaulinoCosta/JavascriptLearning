5b392e688f51a410500256115eeb4e647459cf6d
CaioPaulinoCosta <caiopaulinocosta@hotmail.com>
2023-05-26 16:00:37 -0300

    Exercício 04 | Arrays e Objetos | Realizado

---
+++ estudosJavaScript/Array-e-Objetos/Exercicio-04/script.js
@@ -0,0 +1,16 @@
+let Onibus = {
+    rodas: 8,
+    limiteDePassageiros: 40,
+    portas: 2
+};
+
+delete Onibus.rodas;
+
+console.log(Onibus.rodas);
+
+Onibus.janelas = 20;
+
+console.log(Onibus);
+console.log(Onibus.janelas);
+
+
