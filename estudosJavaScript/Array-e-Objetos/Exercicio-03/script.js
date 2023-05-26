6cbf51a065ce48be65e95270bb0982a6adb3cd96
CaioPaulinoCosta <caiopaulinocosta@hotmail.com>
2023-05-26 15:55:26 -0300

    Exercício 03 | Arrays e Objetos | Realizado

---
+++ estudosJavaScript/Array-e-Objetos/Exercicio-03/script.js
@@ -0,0 +1,11 @@
+let Onibus = {
+    rodas: 8,
+    limiteDePassageiros: 40,
+    portas: 2
+};
+
+console.log(Onibus);
+
+console.log(Onibus.rodas);
+console.log(Onibus.limiteDePassageiros);
+console.log(Onibus.portas);
