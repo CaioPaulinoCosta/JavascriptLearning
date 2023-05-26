da656d3fcbc80e854d90607a361a92896d8f38b5
CaioPaulinoCosta <caiopaulinocosta@hotmail.com>
2023-05-26 17:00:28 -0300

    Exercício 09 | Arrays e Objetos | Realizado

---
+++ estudosJavaScript/Array-e-Objetos/Exercicio-08/script.js
@@ -0,0 +1,7 @@
+const frase = "Eu sou uma frase e vou ser transformada em array!";
+
+const arrfrase = frase.split(" ");
+
+for (let i = 0;i <= frase.length; i++){
+    console.log(arrfrase[i]);
+}    
