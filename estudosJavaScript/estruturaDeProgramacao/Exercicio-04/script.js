906d46708cbebbd5478e8814d722f3e7a350b4c6
CaioPaulinoCosta <caiopaulinocosta@hotmail.com>
2023-05-26 00:13:09 -0300

    Exercício 04 | Estrutura de Programação | Realizado

---
+++ estudosJavaScript/estruturaDeProgramacao/Exercicio-04/script.js
@@ -0,0 +1,26 @@
+let num1 = 2;
+let num2 = 3;
+let num3 = 18;
+let num4 = 0;
+
+let resultado = Math.pow(num1,2);
+
+console.log(resultado);
+
+//
+
+let resultado2 = Math.pow(num2,2);
+
+console.log(resultado2);
+
+//
+
+let resultado3 = Math.pow(num3,2);
+
+console.log(resultado3);
+
+//
+
+let resultado4 = Math.pow(num4,2);
+
+console.log(resultado4);
