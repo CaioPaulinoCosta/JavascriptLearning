950e796ee339312b1c2ed338d7002b6537de432d
CaioPaulinoCosta <caiopaulinocosta@hotmail.com>
2023-05-26 00:23:36 -0300

    Exercício 06 | Estrutura de Programaçao | Realizado

---
+++ estudosJavaScript/estruturaDeProgramacao/Exercicio-06/script.js
@@ -0,0 +1,25 @@
+let nome = "Caio"; let idade = "18"; let cnh = true;
+let nome2 = "Mario"; let idade2 = 15; let cnh2 = false;
+let nome3 = "Wanderley"; let idade3 = 18; let cnh3 = false;
+
+if (idade >= 18 && cnh === true) {
+    console.log(`Parabéns ${nome} você pode dirigir!`);
+} else {
+    console.log(`Sinto muito ${nome} , mas infelizmente você ainda não pode dirigir :/`);
+}
+
+// 
+
+if (idade2 >= 18 && cnh2 === true) {
+    console.log(`Parabéns ${nome2} você pode dirigir!`);
+} else {
+    console.log(`Sinto muito ${nome2}, mas infelizmente você ainda não pode dirigir :/`);
+}
+
+// 
+
+if (idade3 >= 18 && cnh3 === true) {
+    console.log(`Parabéns ${nome3} você pode dirigir!`);
+} else {
+    console.log(`Sinto muito ${nome3}, mas infelizmente você ainda não pode dirigir :/`);
+}
