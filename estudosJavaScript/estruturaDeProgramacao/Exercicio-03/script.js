931036e56497ef83486d244271ff3721279afdc7
CaioPaulinoCosta <caiopaulinocosta@hotmail.com>
2023-05-26 00:09:58 -0300

    Exercício 03 | Estrutura de Programação | Realizado

---
+++ estudosJavaScript/estruturaDeProgramacao/Exercicio-03/script.js
@@ -0,0 +1,15 @@
+const nome = "Caio";
+
+if (nome == "Caio") {
+    console.log(`Oi, ${nome}, muito bom te encontrar por aqui!`);
+} else {
+    console.log("SAI DAQUI!");
+}
+
+const nome2 = "Luis";
+
+if (nome2 == "Caio") {
+    console.log(`Oi, ${nome}, muito bom te encontrar por aqui!`);
+} else {
+    console.log("SAI DAQUI!");
+}
