40859c70c9ab46d16dbb301cf5909f3f4ab8ef0b
CaioPaulinoCosta <caiopaulinocosta@hotmail.com>
2023-05-26 00:16:10 -0300

    Exercício 05 | Estrutura de Programaçao | Realizado

---
+++ estudosJavaScript/estruturaDeProgramacao/Exercicio-05/script.js
@@ -0,0 +1,16 @@
+let velocidade01 = 200; let velocidade02 = 60; let volocidadePermitida = 80;
+
+if(velocidade02 <= volocidadePermitida){
+    console.log("O veiculo anda na velocidade permitida!");
+} else {
+    console.log("O veiculo anda acima da velocidade permitida!");
+}
+
+//
+
+if(velocidade01 <= volocidadePermitida){
+    console.log("O veiculo anda na velocidade permitida!");
+} else {
+    console.log("O veiculo anda acima da velocidade permitida!");
+}
+
